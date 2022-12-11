1987979469c444e2d31482fa2b4706a655c58fe3
wallas938 <drame.sissako001@gmail.com>
2022-12-11 16:35:39 +0100

    first commit

---
+++ src/main/java/fr/greta/domes_server/DomesServerApplication.java
@@ -0,0 +1,13 @@
+package fr.greta.domes_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomesServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomesServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/greta/domes_server/DomesServerApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.greta.domes_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomesServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
