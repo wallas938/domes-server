16f65ed7b59f2f79bbf0655ff9374743750ad4fd
Wallas9_3 <drame.sissako001@gmail.com>
2023-04-24 08:40:42 +0200

    a finir

--- src/main/java/fr/greta/domes_server/entities/AuthenticationTokenResponse.java
+++ src/main/java/fr/greta/domes_server/entities/AuthenticationTokenResponse.java
@@ -1,37 +1,13 @@
 package fr.greta.domes_server.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthenticationTokenResponse {
-    private String access_token;
-    private String refresh_token;
-
-    public AuthenticationTokenResponse(String access_token, String refresh_token) {
-        this.access_token=access_token;
-        this.refresh_token=refresh_token;
-    }
-
-    public AuthenticationTokenResponse() {}
-
-    public String getAccess_token() {
-        return access_token;
-    }
-
-    public void setAccess_token(String access_token) {
-        this.access_token = access_token;
-    }
-
-    public String getRefresh_token() {
-        return refresh_token;
-    }
-
-    public void setRefresh_token(String refresh_token) {
-        this.refresh_token = refresh_token;
-    }
-
-    @Override
-    public String toString() {
-        return "AuthenticationTokenRequest{" +
-                "access_token='" + access_token + '\'' +
-                ", refresh_token='" + refresh_token + '\'' +
-                '}';
-    }
+    private String accessToken;
+    private String refreshToken;
 }
